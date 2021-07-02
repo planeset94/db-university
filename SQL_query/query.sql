b6256db1f8a65da9d0b22e53f15085b2a38e6c76
planeset94 <planeset94@gmail.com>
2021-07-02 17:16:09 +0200

    Query n*3-4

--- SQL_query/query.sql
+++ SQL_query/query.sql
@@ -1,3 +1,3 @@
 1) SELECT * FROM `students` WHERE `date_of_birth` BETWEEN '1990-01-01' AND '1990-12-31';
 2) SELECT * FROM `courses` WHERE `cfu` > 10;
-3)
+3)SELECT * FROM `students` WHERE DATEDIFF(CURDATE(), `date_of_birth`)>= 30;
