72320ce7950976d4c215f42397c21cc0748ad6b8
planeset94 <planeset94@gmail.com>
2021-07-05 17:56:07 +0200

    JOINS: query 1-2

--- Group-JOIN/query.sql
+++ Group-JOIN/query.sql
@@ -18,5 +18,21 @@
     FROM degrees
     GROUP BY  department_id;
 
+-- JOINS:
+-- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+1) SELECT students.name, students.surname, students.fiscal_code, degrees.name
+    FROM students
+    JOIN degrees
+    ON students.degree_id= degrees.id
+    WHERE degrees.name= "Corso di Laurea in Economia";
 
-    
+-- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+2) SELECT degrees.name, degrees.level, degrees.website, degrees.email, degrees.id AS "Codice facoltà", departments.name
+    FROM degrees
+    JOIN departments
+    ON degrees.department_id = departments.id
+    WHERE departments.name="Dipartimento di Neuroscienze"
+
+-- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+3) 
+
