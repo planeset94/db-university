b0ec3f692f65e6ff7cea67349a48289a0d0d49b2
planeset94 <planeset94@gmail.com>
2021-07-05 17:29:05 +0200

    Group-by: query 3-4

--- Group-JOIN/query.sql
+++ Group-JOIN/query.sql
@@ -8,3 +8,15 @@
     FROM teachers
     GROUP BY Edificio;
 
+-- Calcolare la media dei voti di ogni appello d'esame
+3) SELECT exam_id, AVG(vote) as "Media_Voti"
+    FROM exam_student
+    GROUP BY exam_id;
+
+-- Contare quanti corsi di laurea ci sono per ogni dipartimento
+4) SELECT department_id, COUNT(id)
+    FROM degrees
+    GROUP BY  department_id;
+
+
+    
